46b1104280edacd32015327c9a05501144f280f5
Jsinco <jonah@jsinco.dev>
2025-04-23 12:02:43 -0400

    fixup! Add EntityCollideWithEntityEvent

--- paper-api/src/main/java/io/papermc/paper/event/entity/EntityCollideWithEntityEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/entity/EntityCollideWithEntityEvent.java
@@ -2,95 +2,216 @@ package io.papermc.paper.event.entity;
 
 import org.bukkit.entity.Entity;
 import org.bukkit.event.Cancellable;
-import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.entity.EntityEvent;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
-import java.util.Set;
 
 /**
  * Fired when two entities collide with each other.
- * If cancelled, the entities won't get pushed away from each other.
+ * If cancelled, both entities won't get pushed away from each other.
+ * <br />
+ * For specific entity collision results, use {@link #getResult()} and {@link #setResult(Result)}.
+ * <br />
+ * This event will not fire if collisions are disabled in the paper config.
  */
 public class EntityCollideWithEntityEvent extends EntityEvent implements Cancellable {
 
     private static final HandlerList HANDLER_LIST = new HandlerList();
+
     private final Entity collided;
-    private boolean cancelled;
-    private double pushX;
-    private double pushY;
-    private double pushZ;
+    private Result result;
+
+    private double colliderPushX;
+    private double colliderPushY;
+    private double colliderPushZ;
+
+    private double collidedPushX;
+    private double collidedPushY;
+    private double collidedPushZ;
+
 
     @ApiStatus.Internal
-    public EntityCollideWithEntityEvent(@NotNull Entity collider, @NotNull Entity collided, double pushX, double pushY, double pushZ) {
+    public EntityCollideWithEntityEvent(@NotNull Entity collider, @NotNull Entity collided, double colliderPushX, double colliderPushY, double colliderPushZ, double collidedPushX, double collidedPushY, double collidedPushZ) {
         super(collider);
         this.collided = collided;
-        this.pushX = pushX;
-        this.pushY = pushY;
-        this.pushZ = pushZ;
+        this.result = Result.ALLOW;
+
+        this.colliderPushX = colliderPushX;
+        this.colliderPushY = colliderPushY;
+        this.colliderPushZ = colliderPushZ;
+
+        this.collidedPushX = collidedPushX;
+        this.collidedPushY = collidedPushY;
+        this.collidedPushZ = collidedPushZ;
     }
 
+
+    /**
+     * Gets the entity colliding with another entity.
+     *
+     * @return the entity colliding with another entity
+     */
     public Entity getCollider() {
         return this.getEntity();
     }
 
+    /**
+     * Gets the entity that was collided with.
+     *
+     * @return the entity that was collided with
+     */
     public Entity getCollided() {
         return this.collided;
     }
 
     /**
-     * Returns push in the X direction
+     * Returns the collider's push in the X direction
      *
-     * @return push in the X direction
+     * @return the collider's push in the X direction
      */
-    public double getPushX() {
-        return this.pushX;
+    public double getColliderPushX() {
+        return colliderPushX;
     }
 
     /**
-     * Returns push in the Y direction
+     * Returns the collider's push in the Y direction
      *
-     * @return push in the Y direction
+     * @return the collider's push in the Y direction
      */
-    public double getPushY() {
-        return this.pushY;
+    public double getColliderPushY() {
+        return colliderPushY;
     }
 
     /**
-     * Returns push in the Z direction
+     * Returns the collider's push in the Z direction
      *
-     * @return push in the Z direction
+     * @return the collider's push in the Z direction
      */
-    public double getPushZ() {
-        return this.pushZ;
+    public double getColliderPushZ() {
+        return colliderPushZ;
     }
 
     /**
-     * Sets the push in the X direction
+     * Sets the collider's push in the X direction
      *
-     * @param pushX push in the X direction
+     * @param colliderPushX push in the X direction
      */
-    public void setPushX(double pushX) {
-        this.pushX = pushX;
+    public void setColliderPushX(double colliderPushX) {
+        this.colliderPushX = colliderPushX;
     }
 
     /**
-     * Sets the push in the Y direction
+     * Sets the collider's push in the Y direction
      *
-     * @param pushY push in the Y direction
+     * @param colliderPushY push in the Y direction
      */
-    public void setPushY(double pushY) {
-        this.pushY = pushY;
+    public void setColliderPushY(double colliderPushY) {
+        this.colliderPushY = colliderPushY;
     }
 
     /**
-     * Sets the push in the Z direction
+     * Sets the collider's push in the Z direction
      *
-     * @param pushZ push in the Z direction
+     * @param colliderPushZ push in the Z direction
      */
-    public void setPushZ(double pushZ) {
-        this.pushZ = pushZ;
+    public void setColliderPushZ(double colliderPushZ) {
+        this.colliderPushZ = colliderPushZ;
+    }
+
+    /**
+     * Returns the collided entities' push in the X direction
+     *
+     * @return the collided entities' push in the X direction
+     */
+    public double getCollidedPushX() {
+        return this.collidedPushX;
+    }
+
+    /**
+     * Returns the collided entities' push in the Y direction
+     *
+     * @return the collided entities' push in the Y direction
+     */
+    public double getCollidedPushY() {
+        return this.collidedPushY;
+    }
+
+    /**
+     * Returns the collided entities' push in the Z direction
+     *
+     * @return the collided entities' push in the Z direction
+     */
+    public double getCollidedPushZ() {
+        return this.collidedPushZ;
+    }
+
+    /**
+     * Sets the collided entities' push in the X direction
+     *
+     * @param collidedPushX push in the X direction
+     */
+    public void setCollidedPushX(double collidedPushX) {
+        this.collidedPushX = collidedPushX;
+    }
+
+    /**
+     * Sets the collided entities' push in the Y direction
+     *
+     * @param collidedPushY push in the Y direction
+     */
+    public void setCollidedPushY(double collidedPushY) {
+        this.collidedPushY = collidedPushY;
+    }
+
+    /**
+     * Sets the collided entities' push in the Z direction
+     *
+     * @param collidedPushZ push in the Z direction
+     */
+    public void setCollidedPushZ(double collidedPushZ) {
+        this.collidedPushZ = collidedPushZ;
+    }
+
+    /**
+     * Returns the result of this event. By default, the result will be {@link Result#ALLOW}.
+     *
+     * @return the result of this event
+     */
+    public Result getResult() {
+        return this.result;
+    }
+
+    /**
+     * Sets the result of this event. This will change the behavior of the collision.
+     *
+     * @param result the result to set
+     */
+    public void setResult(Result result) {
+        this.result = result;
+    }
+
+
+    /**
+     * Returns true only if both entities will NOT be pushed away from each other.
+     * For specific entity collision results, use {@link #getResult()}.
+     *
+     * @return true if the event is cancelled
+     */
+    @Override
+    public boolean isCancelled() {
+        return this.result == Result.DENY;
+    }
+
+    /**
+     * Sets the cancellation state of this event.
+     * For specific entity collision results, use {@link #setResult(Result)}.
+     *
+     * @param cancel true if you wish to cancel this event
+     */
+    @Override
+    public void setCancelled(final boolean cancel) {
+        this.result = cancel ? Result.DENY : Result.ALLOW;
     }
 
     @Override
@@ -103,13 +224,22 @@ public class EntityCollideWithEntityEvent extends EntityEvent implements Cancell
         return HANDLER_LIST;
     }
 
-    @Override
-    public boolean isCancelled() {
-        return cancelled;
-    }
-
-    @Override
-    public void setCancelled(final boolean cancel) {
-        this.cancelled = cancel;
+    public enum Result {
+        /**
+         * Allows both collisions and both entities will be pushed.
+         */
+        ALLOW,
+        /**
+         * Denies both collisions and neither entity will be pushed.
+         */
+        DENY,
+        /**
+         * Denies the collision for only the collider.
+         */
+        DENY_COLLIDER_COLLISION,
+        /**
+         * Denies the collision for only the entity that was collided with.
+         */
+        DENY_COLLIDED_COLLISION
     }
 }
